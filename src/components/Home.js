c619b2d08a3eaf12ec69e8f38946a46e96162558
David Pavlovski <david.pavlovski@hotmail.com>
2022-02-24 21:21:22 +0100

    added base functionality in home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import COUNTRIES_API from '../COUNTRIES_API';
 export default function Home(){
-   return <React.Fragment>Home Page</React.Fragment>;
+   const [ loading, setLoading ] = useState(false);
+   const [ error, setError ] = useState(false);
+   const [ countries, setCountries ] = useState([]);
+
+   const fetchCountries = async () => {
+      try {
+         setError(false);
+         setLoading(true);
+         const data = await COUNTRIES_API.fetchCountries();
+         setCountries(data);
+         setLoading(false);
+      } catch (e) {
+         setError(true);
+      }
+   };
+
+   useEffect(() => {
+      fetchCountries();
+   }, []);
+   return (
+      <React.Fragment>
+         {countries.map((country) => <p key={country.name.official}>{country.name.official}</p>)}
+      </React.Fragment>
+   );
 }
